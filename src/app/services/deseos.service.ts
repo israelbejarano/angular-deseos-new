d8c0f73d50f3af2359877f188bf3699868a2400a
israel <israel.bejarano.a@gmail.com>
2019-03-05 22:48:19 +0100

    DeseosService creation

---
+++ src/app/services/deseos.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { Lista } from '../models/lista.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DeseosService {
+
+  lista: Lista[] = [];
+
+  constructor() {
+    console.log('Servicio inicializando');
+  }
+}
