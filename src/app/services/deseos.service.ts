f27d23fc2540d21c5c36800057bcafa1e0d819e5
israel <israel.bejarano.a@gmail.com>
2019-03-06 22:10:19 +0100

    obtenerLista method

--- src/app/services/deseos.service.ts
+++ src/app/services/deseos.service.ts
@@ -15,9 +15,15 @@ export class DeseosService {
   }
 
   crearLista(titulo: string) {
-    const lista = new Lista(titulo);
-    this.listas.push(lista);
+    const nuevaLista = new Lista(titulo);
+    this.listas.push(nuevaLista);
     this.guardarStorage();
+    return nuevaLista.id;
+  }
+
+  obtenerLista(id: string | number) {
+    id = Number(id);
+    return this.listas.find(listaData => listaData.id === id);
   }
 
   guardarStorage() {
