9635f7885adbcde48d90c25d3a5a9b67c4058ccf
israel <israel.bejarano.a@gmail.com>
2019-03-06 22:14:29 +0100

    add routing

--- src/app/pages/tab1/tab1.page.ts
+++ src/app/pages/tab1/tab1.page.ts
@@ -13,7 +13,6 @@ export class Tab1Page {
               private alertCtrl: AlertController) { }
 
   async agregarLista() {
-    // this.router.navigateByUrl('/tabs/tab1/agregar');
     const alert = await this.alertCtrl.create({
       header: 'Nueva lista',
       inputs: [{
@@ -36,7 +35,8 @@ export class Tab1Page {
             if (data.titulo.length === 0) {
               return;
             }
-            this.deseosService.crearLista(data.titulo);
+            const listaId = this.deseosService.crearLista(data.titulo);
+            this.router.navigateByUrl(`/tabs/tab1/agregar/${listaId}`);
           }
         }
     ]
